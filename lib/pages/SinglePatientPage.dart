b3aa8229cbba90a2fce796acb97fcf008335daee
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2024-10-16 22:12:30 -0300

    Ahora si funciona

--- lib/pages/SinglePatientPage.dart
+++ lib/pages/SinglePatientPage.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/classes/Patient.dart';
+import 'package:flutter_application_1/classes/PatientDescription.dart';
 import 'package:flutter_application_1/classes/PatientResponse.dart';
 import 'package:flutter_application_1/classes/SinglePatientPageArguments.dart';
 
@@ -17,28 +18,6 @@ class SinglePatientPage extends StatefulWidget {
 }
 
 class _SinglePatientPageState extends State<SinglePatientPage> {
-  late Future<List<dynamic>> data;
-
-  get http => null;
-
-  @override
-  void initState() {
-    super.initState();
-    data = fetchData() as Future<List>; // Llamada a la API
-  }
-
-  Future<Patient> fetchData() async {
-    final response = await http.get(
-        Uri.parse("https://tup-labo-4-grupo-15.onrender.com/api/v1/patients"));
-
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = jsonDecode(response.body);
-      return PatientResponse.fromJson(data).patient;
-    } else {
-      throw Exception('Error al cargar los datos');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     // Extract the arguments from the current ModalRoute
@@ -51,7 +30,7 @@ class _SinglePatientPageState extends State<SinglePatientPage> {
         title: Text("Paciente"),
       ),
       body: Center(
-        child: Text(args.id),
+        child: PatientDescription(id: args.id),
       ),
     );
   }
